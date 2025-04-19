81dcc6abfe7e4a897675867fc505f2dbe0418f03
ne-supanat <ne-supanat@users.noreply.github.com>
2025-04-19 14:50:18 +0000

    deploy: 87c021ccd408bd731ca26501dc32f04545a9cd25

---
+++ data/tag_categories.js
@@ -0,0 +1,16 @@
+define({
+  tagCategories: {
+    application: {
+      description: "paper application field",
+    },
+    category: {
+      description: "paper idea theme field",
+    },
+    simulation: {
+      description: "paper simulation paradigm",
+    },
+    ml: {
+      description: "paper machine learning method",
+    },
+  },
+});
